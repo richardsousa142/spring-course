bc94b564a18dabbafc753098291d6dd22e3bb674
richardsousa142 <richardmatias12345@gmail.com>
2021-10-11 21:51:00 -0300

    create user save and update dto

---
+++ src/main/java/com/springcourse/dto/UserSavedto.java
@@ -0,0 +1,41 @@
+package com.springcourse.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.Role;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserSavedto {
+	
+	@NotBlank(message = "Name required")
+	private String name;
+	
+	@Email(message = "Invalid email address")
+	private String email;
+	
+	@Size(min = 7, max = 100, message = "Password must be between 7 and 99")
+	private String password;
+	
+	@NotNull(message = "Role required")
+	private Role roleOfUser;
+	
+	private List<Request> requests = new ArrayList<Request>();
+	private List<RequestStage> stageOfRequests = new ArrayList<RequestStage>();
+
+	public User transformaToUser() {
+		User user = new User(null, this.name, this.email, this.password, this.roleOfUser, this.requests, this.stageOfRequests);
+		return user;
+	}
+}
