bda6983c80b45e9c4e1583f0b19f8fcd1688f7ca
richardsousa142 <richardmatias12345@gmail.com>
2021-10-13 21:46:54 -0300

    config deployable war file

--- src/main/java/com/springcourse/SpringCourseApplication.java
+++ src/main/java/com/springcourse/SpringCourseApplication.java
@@ -2,10 +2,17 @@ package com.springcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCourseApplication {
-
+public class SpringCourseApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringCourseApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
 	}
