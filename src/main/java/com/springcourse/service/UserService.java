6cae773b823c267277dc6ed7959bc68a2121f161
richardsousa142 <richardmatias12345@gmail.com>
2021-10-15 21:58:57 -0300

    implement user details service

--- src/main/java/com/springcourse/service/UserService.java
+++ src/main/java/com/springcourse/service/UserService.java
@@ -1,5 +1,6 @@
 package com.springcourse.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.springcourse.domain.User;
@@ -17,7 +23,7 @@ import com.springcourse.repository.UserRepository;
 import com.springcourse.service.util.HashUtil;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -56,4 +62,19 @@ public class UserService {
 	public int updateRoleOfUser(User user) {
 		return userRepository.updateRoleOfUser(user.getId(), user.getRoleOfUser().toString());
 	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> result = userRepository.findByEmail(username);
+		if(!result.isPresent()) throw new UsernameNotFoundException("Usuario com com email: "+username+" não existe.");
+		User user = result.get();
+		
+		List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE "+user.getRoleOfUser().name()));
+		
+		org.springframework.security.core.userdetails.User userSpring = 
+				new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+		
+		return userSpring;
+	}
+	
 }
