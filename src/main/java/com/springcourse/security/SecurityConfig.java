7c53a1af1df32bf58a0494d74ac416f0c457cc43
richardsousa142 <richardmatias12345@gmail.com>
2021-10-15 23:44:44 -0300

    create jwt manager

--- src/main/java/com/springcourse/security/SecurityConfig.java
+++ src/main/java/com/springcourse/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.springcourse.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,9 +23,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(customPasswordEncoder);
 	}
+	
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 	@Override
-	protected AuthenticationManager authenticationManagerBean() throws Exception {
-		// TODO Auto-generated method stub
+	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
 }
