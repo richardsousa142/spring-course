d1f269136520eeb38183a19ef72d91a2e0fe0357
richardsousa142 <richardmatias12345@gmail.com>
2021-10-15 23:44:19 -0300

    add jwt on login response

--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -1,9 +1,11 @@
 package com.springcourse.resource;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
-import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,18 +31,17 @@ import com.springcourse.dto.UserUpdateRoledto;
 import com.springcourse.dto.UserUpdatedto;
 import com.springcourse.model.PageModel;
 import com.springcourse.model.PageRequestModel;
+import com.springcourse.security.JwtManager;
 import com.springcourse.service.RequestService;
 import com.springcourse.service.UserService;
 
 @RestController
 @RequestMapping(value="users")
 public class UserResource {
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private RequestService requestService;
-	@Autowired
-	private AuthenticationManager authManager;
+	@Autowired private UserService userService;
+	@Autowired private RequestService requestService;
+	@Autowired private AuthenticationManager authManager;
+	@Autowired private JwtManager jwtManager;
 	
 	@PostMapping
 	public ResponseEntity<User> saveUser(@RequestBody @Valid UserSavedto userdto){
@@ -81,11 +81,24 @@ public class UserResource {
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<User> loginUser(@RequestBody @Valid UserLogindto userLogindto){
+	public ResponseEntity<String> loginUser(@RequestBody @Valid UserLogindto userLogindto){
 		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userLogindto.getEmail(), userLogindto.getPassword());
 		Authentication auth =  authManager.authenticate(token);
 		SecurityContextHolder.getContext().setAuthentication(auth);
-		return ResponseEntity.ok(null);
+		
+		org.springframework.security.core.userdetails.User userSpring = 
+								(org.springframework.security.core.userdetails.User) auth.getPrincipal();
+		
+		String email = userSpring.getUsername();
+		List<String> roles = userSpring.getAuthorities()
+								.stream()
+								.map(authority -> authority.getAuthority())
+								.collect(Collectors.toList());
+		
+		String jwt = jwtManager.creteToken(email, roles);
+		
+		
+		return ResponseEntity.ok(jwt);
 	} 
 	
 	@PatchMapping("/role/{id}")
