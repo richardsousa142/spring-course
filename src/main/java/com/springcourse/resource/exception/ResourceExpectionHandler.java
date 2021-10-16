30be829fa4410386b1e8278a87d038beeab8dffc
richardsousa142 <richardmatias12345@gmail.com>
2021-10-16 00:02:39 -0300

    config bad credentials exception handler

--- src/main/java/com/springcourse/resource/exception/ResourceExpectionHandler.java
+++ src/main/java/com/springcourse/resource/exception/ResourceExpectionHandler.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -36,5 +37,12 @@ public class ResourceExpectionHandler extends ResponseEntityExceptionHandler{
 		ApiErrorList error = new ApiErrorList(HttpStatus.BAD_REQUEST.value(), "Invalid field(s)", new Date(), errors);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
+	
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<ApiError> handlerBadCredentialsException(BadCredentialsException ex){
+		ApiError error = new ApiError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+	}
+	
 }
  
