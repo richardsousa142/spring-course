9ba073078c9fcd88a7903fd220f5138f68d952e4
richardsousa142 <richardmatias12345@gmail.com>
2021-10-15 23:08:50 -0300

    authentitication manager as Bean

--- src/main/java/com/springcourse/security/SecurityConfig.java
+++ src/main/java/com/springcourse/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.springcourse.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,9 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	private UserService userService;
 	@Autowired
 	private CustomPasswordEncoder customPasswordEncoder;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(customPasswordEncoder);
-		
+	}
+	@Override
+	protected AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
 	}
 }
